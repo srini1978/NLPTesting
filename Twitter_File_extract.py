64a3d38bd6d8563b73f95a1b03d4e07cad4e74ac
Srinivasan <srini1978@gmail.com>
2015-02-09 16:11:22 +0530

    test changes

--- Twitter_File_extract.py
+++ Twitter_File_extract.py
@@ -1,6 +1,7 @@
 
 import re
-
+import nltk
+import nltk.classify
 #start process_tweet
 def processTweet(tweet):
     # process the tweets
@@ -67,43 +68,62 @@ def getFeatureVector(tweet):
     return featureVector
 #end
 
-#Read the tweets one by one and process it
-
+#Read the training data tweets one by one and process it
 st = open('stopwords.txt', 'r')
 stopWords = getStopWordList('stopwords.txt')
 
-#fp = open('twitter_data.txt', 'r')
-import csv
-with open('sample_twitter_data.csv', 'r') as csvfile:
-    fp = csv.reader(open(csvfile, 'rb'), delimiter=',', quotechar='|')
+pos_tweets = [('I love this car', 'positive'), 
+              ('This view is amazing', 'positive'), 
+              ('I feel great this morning', 'positive'), 
+             ('I am so excited about the concert', 'positive'), 
+              ('He is my best friend', 'positive')] 
 
-tweets=[]
-for row in fp:
-    sentiment = row[0]
-    tweet = row[1]
-    processedTweet = processTweet(tweet)
+
+neg_tweets = [('I do not like this car', 'negative'), 
+              ('This view is horrible', 'negative'), 
+              ('I feel tired this morning', 'negative'), 
+              ('I am not looking forward to the concert', 'negative'), 
+              ('He is my enemy', 'negative')] 
+
+
+tweets = [] 
+for (words, sentiment) in pos_tweets + neg_tweets: 
+    processedTweet = processTweet(words)
     featureVector = getFeatureVector(processedTweet)
     tweets.append((featureVector, sentiment));
+    # this is the list of features extracted from training set
     print(featureVector)
-
 #end loop
 
+# from this feature list , do a extract of featurs
+def extract_features(document): 
+    document_words = set(document) 
+    features = {} 
+    for word in featureVector: 
+        features['contains(%s)' % word] = (word in document_words) 
+        return features
 
-fc = open('twitter_processed_data.txt','w')
+
+training_set = nltk.classify.apply_features(extract_features, tweets)
+classifier = nltk.classify.NaiveBayesClassifier.train(training_set)
+tweet = 'Larry is an enemy idiot ' 
+result= classifier.classify(extract_features(tweet.split()))
+print(result)
 
 
+
+
+
+#fc = open('twitter_processed_data.txt','w')
 #while line:
     #processedTweet = processTweet(line)
    # featureVector = getFeatureVector(processedTweet)
    # print (featureVector)
-for item in featureVector:
-    fc.write("%s\n" % item)
+#for item in featureVector:
+ #   fc.write("%s\n" % item)
 
     #line = fp.readline()
 #end loop
-st.close()
-fc.close()
-fp.close()
 
 
 
